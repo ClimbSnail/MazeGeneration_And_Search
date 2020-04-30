d71c040f61f8d260dcdad537c2458b4823e13701
heqi <heqi@sunkaisens.com>
2020-04-30 13:05:37 +0800

    no message

---
+++ Maze/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Maze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeight = 30;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 30;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 419);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(168, 428);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "刷新地图";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(95, 428);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(57, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "32";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 433);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "输入地图大小";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(544, 428);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "规划路径";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(247, 428);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "确定起始点";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(337, 428);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "确定目的地";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(418, 432);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 16);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Display Process";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 460);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Maze";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Maze/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace Maze
+{
+    public partial class Form1 : Form
+    {
+        //
+        public int sizelong;
+        public int colCount;
+        public int rowCount;
+        // 所在地坐标
+        public int localX;
+        public int localY;
+        // 目的地坐标
+        public int remoteX;
+        public int remoteY;
+
+
+        int dataGridView1MaxWidth;
+        int dataGridView1MaxLength;
+
+        // 存放生成的迷宫数据
+        const int size = 1000;
+        bool[][] Maze = new bool[size][];
+
+        // 找到迷宫的标志位
+        public bool findFlag;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 初始化迷宫数据
+            for (int i = 0; i < size; i++)
+            {
+                Maze[i] = new bool[size];
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+                return;
+            // 清空数据
+            this.dataGridView1.Rows.Clear();
+            this.dataGridView1.Columns.Clear();
+            // 得到矩阵的大小
+            sizelong = int.Parse(textBox1.Text);
+            colCount = sizelong;
+            rowCount = sizelong;
+            if (true)
+            {
+                dataGridView1MaxWidth = this.dataGridView1.Width;
+                dataGridView1MaxLength = this.dataGridView1.Height;
+                int rangeWidth = (dataGridView1MaxWidth / colCount) < (dataGridView1MaxLength / rowCount) ?
+                    (dataGridView1MaxWidth / colCount) : (dataGridView1MaxLength / rowCount);
+
+                this.dataGridView1.ColumnHeadersVisible = false;
+                this.dataGridView1.RowHeadersVisible = false;
+                //列宽:
+                int colWidth = rangeWidth;
+                //this.dataGridView1.Width = colWidth * colCount+3 ;
+                //行高:
+                int rowHeight = rangeWidth;
+                //this.dataGridView1.Height = rangeWidth * rowCount + 50;
+                for (int j = 0; j < colCount; j++)
+                {
+                    DataGridViewTextBoxColumn dc = new DataGridViewTextBoxColumn();
+                    dc.Width = colWidth;
+                    dataGridView1.Columns.Add(dc);
+                }
+                for (int i = 0; i < rowCount; i++)
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    row.Height = rowHeight;
+                    dataGridView1.Rows.Add(row);
+                }
+                //禁止用户改变DataGridView1所有行的行高 
+                dataGridView1.AllowUserToResizeRows = false;
+                //禁止用户改变DataGridView1所有列的列宽 
+                dataGridView1.AllowUserToResizeColumns = false;
+
+                // 创建迷宫
+                CreateMaze();
+                // 及时更新
+                dataGridView1.Refresh();
+            }
+        }
+
+        private void CreateMaze()
+        {
+            // 清空迷宫数据
+            for (int i = 0; i < sizelong; i++)
+            {
+                for (int j = 0; j < sizelong; j++)
+                    Maze[i][j] = false;
+            }
+            // true表示为路  false表示为墙
+            //最外围设置为路，可以有效的保护里面一层墙体，并防止挖出界
+            for (int i = 0; i< sizelong; i++) {
+		        Maze[i][0] = true;
+		        Maze[0][i] = true;
+		        Maze[sizelong - 1][i] = true;
+		        Maze[i][sizelong - 1] = true;
+	        }
+
+            Random rd = new Random();
+            
+            //墙队列，包括X , Y
+            ArrayList X = new ArrayList();
+            ArrayList Y = new ArrayList();
+
+            //任取初始值
+            X.Add(2);
+	        Y.Add(2);
+
+	            //当墙队列为空时结束循环
+	        while (X.Count != 0) {
+		            //在墙队列中随机取一点
+		        int r = rd.Next() % X.Count;
+                int x = (int)X[r];
+                int y = (int)Y[r];
+
+                //判读上下左右四个方向是否为路
+                int count = 0;
+		        for (int i = x - 1; i<x + 2; i++) {
+			        for (int j = y - 1; j<y + 2; j++) {
+				        if (Math.Abs(x - i) + Math.Abs(y - j) == 1 && Maze[i][j]) {
+					        count++;
+				        }
+			        }
+		        }
+		        if (count <= 1) {
+			        Maze[x][y] = true;
+			        //在墙队列中插入新的墙
+			        for (int i = x - 1; i<x + 2; i++) {
+				        for (int j = y - 1; j<y + 2; j++) {
+					        if (Math.Abs(x - i) + Math.Abs(y - j) == 1 && !Maze[i][j]) {
+						        X.Add(i);
+						        Y.Add(j);
+					        }
+				        }
+			        }
+		        }
+
+		        //删除当前墙
+		        X.RemoveAt( r);
+		        Y.RemoveAt( r);
+	        }
+
+	        //设置迷宫进出口
+	        Maze[2][1] = true;
+	        for (int i = sizelong - 3; i >= 0; i--) {
+		        if (Maze[i][sizelong - 3] == true) {
+			        Maze[i][sizelong - 2] = true;
+			        break;
+		        }
+	        }
+
+            //改变迷宫的dataGridView颜色
+            for (int i = 0; i< colCount; i++){
+		        for (int j = 0; j< rowCount; j++)
+                {
+                    if (Maze[i][j])
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.White;
+                    else
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.Black;
+                }
+	        }
+
+            // 设置默认所在地
+            localX = 1;
+            localY = 2;
+            dataGridView1.Rows[localY].Cells[localX].Style.BackColor = Color.Yellow;
+            this.dataGridView1.Rows[localY].Cells[localX].Selected = true;
+
+            // 设置默认目的地
+            remoteX = sizelong-2;
+            remoteY = sizelong-3;
+            dataGridView1.Rows[remoteY].Cells[remoteX].Style.BackColor = Color.Red;
+
+        }
+
+        private bool CheckIsLegal(int x, int y)
+        {
+            if (x >= 0 && y >= 0 && x < sizelong && y < sizelong)
+                return true;
+            else
+                return false;
+        }
+        private void DFS()
+        {
+            findFlag = false;
+            const int size = 100;
+            int[][] isArrive = new int[size][];
+            for (int i = 0; i < size; i++)
+            {
+                isArrive[i] = new int[size];
+                for (int j = 0; j < size; j++)
+                    isArrive[i][j] = 0;
+            }
+            
+            //墙队列，包括X , Y
+            ArrayList X = new ArrayList();
+            ArrayList Y = new ArrayList();
+
+            //任取初始值
+            X.Add(localX);
+            Y.Add(localY);
+            isArrive[localX][localY] = 1;
+
+            int [] aroundA = new int[4] {1, 0, 0 ,-1 };
+            int [] aroundB = new int[4] {0, 1, -1, 0 };
+
+            while (X.Count != 0 && !findFlag)
+            {
+                int x = (int)X[X.Count-1];
+                int y = (int)Y[Y.Count-1];
+                int tmpx;
+                int tmpy;
+                bool nowTimeSucc = false;
+                for (int i = 0; i < 4; i++)
+                {
+                    tmpx = x + aroundA[i];
+                    tmpy = y + aroundB[i];
+                    if (!CheckIsLegal(tmpx, tmpy))
+                        continue;
+
+                    if (tmpx == remoteX && tmpy == remoteY)
+                    {
+                        findFlag = true;
+                        break;
+                    }
+                    if (isArrive[tmpx][tmpy] == 1)
+                        continue;
+                    if (this.dataGridView1.Rows[tmpy].Cells[tmpx].Style.BackColor == Color.White)
+                    {
+                        X.Add(tmpx);
+                        Y.Add(tmpy);
+                        isArrive[tmpx][tmpy] = 1;
+                        nowTimeSucc = true;
+                        this.dataGridView1.Rows[tmpy].Cells[tmpx].Style.BackColor = Color.Green;
+                        if( checkBox1.Checked == true )
+                        {
+                            // 及时更新
+                            dataGridView1.Refresh();
+                        }
+                        break ;
+                    }
+                }
+                // 本轮是否添加成功
+                if(nowTimeSucc == false && !findFlag)
+                {
+                    //isArrive[x][y] = 0;
+                    this.dataGridView1.Rows[y].Cells[x].Style.BackColor = Color.White;
+                    X.RemoveAt(X.Count - 1);
+                    Y.RemoveAt(Y.Count - 1);
+                    if (checkBox1.Checked == true)
+                    {
+                        // 及时更新
+                        dataGridView1.Refresh();
+                    }
+                }
+                else
+                    nowTimeSucc = false;
+             }
+
+            int cnt = X.Count;
+            for(int i=1; i<cnt; i++ )
+            {
+                int x = (int)X[i];
+                int y = (int)Y[i];
+                this.dataGridView1.Rows[y].Cells[x].Style.BackColor = Color.Green;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DFS();
+            // 及时更新
+            dataGridView1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 恢复上一个状态
+            this.dataGridView1.Rows[localY].Cells[localX].Style.BackColor = Maze[localY][localX] ? Color.White : Color.Black;
+            // 更新当前选择的单元格
+            localX = this.dataGridView1.CurrentCell.ColumnIndex;
+            localY = this.dataGridView1.CurrentCell.RowIndex;
+
+            this.dataGridView1.Rows[localY].Cells[localX].Selected = false;
+            this.dataGridView1.Rows[localY].Cells[localX].Style.BackColor = Color.Yellow;
+            dataGridView1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // 恢复上一个状态
+            this.dataGridView1.Rows[remoteY].Cells[remoteX].Style.BackColor = Maze[remoteY][remoteX]? Color.White : Color.Black;
+            // 更新当前选择的单元格
+            remoteX = this.dataGridView1.CurrentCell.ColumnIndex;
+            remoteY = this.dataGridView1.CurrentCell.RowIndex;
+
+            this.dataGridView1.Rows[remoteY].Cells[remoteX].Selected = false;
+            this.dataGridView1.Rows[remoteY].Cells[remoteX].Style.BackColor = Color.Red;
+            dataGridView1.Refresh();
+        }
+    }
+}
